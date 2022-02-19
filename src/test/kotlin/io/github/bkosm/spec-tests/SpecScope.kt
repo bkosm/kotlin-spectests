f63cac4c0139be80c7d3649dda16fb1973467aed
bart-kosmala <bartoshmen@gmail.com>
2022-02-19 14:40:41 +0100

    private scope constructor

--- src/test/kotlin/io/github/bkosm/spec-tests/SpecScope.kt
+++ src/test/kotlin/io/github/bkosm/spec-tests/SpecScope.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import java.util.concurrent.CopyOnWriteArrayList
 
-class SpecScope {
+class SpecScope private constructor() {
     private val testSpecs = CopyOnWriteArrayList<DynamicTest>()
 
     fun it(testName: String, testBody: () -> Unit) {
