8fdd028fbfcc127254f3bf72b04eed85a3e1cf54
bart-kosmala <bartoshmen@gmail.com>
2022-02-19 15:26:35 +0100

    prepare publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
+    `maven-publish`
 }
 
-group = "io.github.bkosm.spec-tests"
-version = "0.1.1-SNAPSHOT"
+val versionString = "0.1.1-SNAPSHOT"
+val groupIdString = "io.github.bkosm"
+val artifactIdString = "spec-tests"
+
+group = "$groupIdString.$artifactIdString"
+version = versionString
 
 repositories {
     mavenCentral()
@@ -21,4 +26,16 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = groupIdString
+            artifactId = artifactIdString
+            version = versionString
+
+            from(components["java"])
+        }
+    }
 }
